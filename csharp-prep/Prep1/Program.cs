28591d6e6d9c2b2790d1f21bf57ac6d439346969
nicholelopezramos <nicholelopez587@gmail.com>
2024-09-23 15:05:20 +0800

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.Write($"Your name is {name}, {name} {lastname}.");
+
     }
 }
