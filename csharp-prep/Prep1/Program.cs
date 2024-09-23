39067cc237b14eb36a2d5ada940fa0262e4eaa29
nicholelopezramos <nicholelopez587@gmail.com>
2024-09-23 15:08:59 +0800

    added space

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string name = Console.ReadLine();
 
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
 
         Console.Write($"Your name is {name}, {name} {lastname}.");
