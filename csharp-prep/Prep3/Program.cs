cf5f0e6ebe35bb449a360928a6f3ec94c875a9c3
nicholelopezramos <nicholelopez587@gmail.com>
2024-09-24 23:59:20 +0800

    prep4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -29,6 +29,7 @@ class Program
             else
             {
                 Console.WriteLine("You guessed it!");
+
             }
             
         }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int input = -1; 
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Input loop
+        while (input != 0)
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)  // Append to the list if it's not 0
+            {
+                numbers.Add(input);
+            }
+        }
+
+        // Core Requirements
+        // Compute the sum
+        int sum = numbers.Sum();
+        
+        // Compute the average
+        double average = numbers.Average();
+        
+        // Find the largest number
+        int max = numbers.Max();
+
+        // Output results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+    
     }
 }
