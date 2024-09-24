8351a11a27f3dfbd46b1fb25893372ec1ceeaba3
nicholelopezramos <nicholelopez587@gmail.com>
2024-09-24 18:00:17 +0800

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomNumber = new Random();
+        int ranNumber = randomNumber.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != ranNumber)
+
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (ranNumber > guess)
+
+        {
+            Console.WriteLine("Higher");
+            }
+            else if (ranNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            
+        }
     }
 }
